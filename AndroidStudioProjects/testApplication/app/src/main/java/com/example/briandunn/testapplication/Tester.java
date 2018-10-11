70e7cd8645b3c862863f9d5c7351a81aa2602502
Dunnb18 <dunnb18@students.ecu.edu>
2018-10-11 09:10:09 -0400

    first commit

---
+++ AndroidStudioProjects/testApplication/app/src/main/java/com/example/briandunn/testapplication/Tester.java
@@ -0,0 +1,13 @@
+package com.example.briandunn.testapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tester extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tester);
+    }
+}
